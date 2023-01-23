cc4da8dea55d651d9fe7824066f7b80492c8e885
ahmedmustahid <einsteinium25@gmail.com>
2023-01-23 13:06:39 +0900

    replace absolute paths by relative

--- inference/infer_cocotiny.py
+++ inference/infer_cocotiny.py
@@ -7,13 +7,13 @@ import os
 import cv2
 
 
-root_dir = "/home/ahmed/work/mmpose/"
-root_dir2 = "/home/ahmed/work/mmdetection/"
+root_dir = ".."
+root_dir2 = "../../mmdetection/"
 cfg = Config.fromfile(os.path.join(root_dir, 'configs/cocotiny/hrnet_w32_cocotiny_256x192.py'))
 pose_checkpoint = os.path.join(root_dir, 'work_dirs/hrnet_w32_coco_tiny_256x192/latest.pth')
 det_config = os.path.join(root_dir2, 'configs/yolox/yolox_tiny_8x8_300e_coco.py')
 #det_config = os.path.join(root_dir, 'demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py')
-det_checkpoint = '/home/ahmed/work/mmdetection/checkpoints/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth'
+det_checkpoint = '../../mmdetection/checkpoints/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth'
 #det_checkpoint = '/home/ahmed/work/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
 pose_model = init_pose_model(cfg, pose_checkpoint)
